ed6ae2a9a0e08023c29b673636d63ced99dd79b5
Wat <wat_wtanuw@hotmail.com>
2022-01-21 02:47:43 +0700

    Change module call

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,43 @@
 import { NativeModules, Platform } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react-native-rakuten-rewark' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+// const LINKING_ERROR =
+//   `The package 'react-native-rakuten-rewark' doesn't seem to be linked. Make sure: \n\n` +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo managed workflow\n';
 
-const RakutenRewark = NativeModules.RakutenRewark
-  ? NativeModules.RakutenRewark
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+// const RakutenRewark = NativeModules.RakutenRewark
+//   ? NativeModules.RakutenRewark
+//   : new Proxy(
+//       {},
+//       {
+//         get() {
+//           throw new Error(LINKING_ERROR);
+//         },
+//       }
+//     );
 
-export default RakutenRewark;
-export { RakutenRewark };
+// export default RakutenRewark;
+// export { RakutenRewark };
 
-export function multiply(a: number, b: number): Promise<number> {
+const { RakutenRewark } = NativeModules
+interface CalendarInterface {
+   toast(message: string): void;
+}
+export default RakutenRewark as CalendarInterface;
+
+export function mmultiply(a: number, b: number): Promise<number> {
   return RakutenRewark.multiply(a, b);
 }
 
-export function toast(a: string) {
+export function ttoast(a: string) {
   return RakutenRewark.toast(a);
 }
 
-export function showL(a: string) {
+export function sshowL(a: string) {
   return RakutenRewark.showL(a);
 }
 
-export function showS(a: string) {
+export function sshowS(a: string) {
   return RakutenRewark.showS(a);
 }
