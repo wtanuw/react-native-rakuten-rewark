036c28f8ef7959f7868c666cf50525f4e3649114
Wat <wat_wtanuw@hotmail.com>
2022-01-21 22:01:08 +0700

    Add ios

--- ios/RakutenRewark.m
+++ ios/RakutenRewark.m
@@ -1,4 +1,11 @@
 #import "RakutenRewark.h"
+#import "IOSNativeToast.h"
+
+@interface RakutenRewark()
+
+@property (nonatomic, retain) IOSNativeToast *toast;
+
+@end
 
 @implementation RakutenRewark
 
@@ -14,6 +21,44 @@ RCT_REMAP_METHOD(multiply,
   NSNumber *result = @([a floatValue] * [b floatValue]);
 
   resolve(result);
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.toast = [[IOSNativeToast alloc] init];
+    }
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_METHOD(show:(NSString *)text)
+{
+    [self.toast showToast:text];
+}
+
+RCT_EXPORT_METHOD(toast:(NSString *)text)
+{
+    [self.toast showToast:text];
+}
+
+RCT_EXPORT_METHOD(showL:(NSString *)text)
+{
+    [self.toast showToast:text duration:3.5];
+}
+
+RCT_EXPORT_METHOD(showS:(NSString *)text)
+{
+    [self.toast showToast:text duration:2.0];
 }
 
 @end
