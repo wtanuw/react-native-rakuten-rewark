9f07eb007e21f7521853ec65635f5338a73309be
Wat <wat_wtanuw@hotmail.com>
2022-01-21 12:17:48 +0700

    Change import

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import { NativeModules, Platform } from 'react-native';
+// import { NativeModules, Platform } from 'react-native';
+import { NativeModules } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react-native-rakuten-rewark' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+// const LINKING_ERROR =
+//   `The package 'react-native-rakuten-rewark' doesn't seem to be linked. Make sure: \n\n` +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo managed workflow\n';
 
 // const RakutenRewark = NativeModules.RakutenRewark
 //   ? NativeModules.RakutenRewark
